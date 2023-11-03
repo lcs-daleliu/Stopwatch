be7d89d28ce4e8410fabacf1fa3b79447feaa9e7
Dale Liu <YLiu@lcs.on.ca>
2023-11-03 09:03:53 -0400

    Initial Commit

---
+++ Stop Wacth/Stop_WacthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_WacthApp.swift
+//  Stop Wacth
+//
+//  Created by Yiren LIU on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct Stop_WacthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
