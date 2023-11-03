2f473000de2ba9ebf36a762e19a02ef045acd6db
Dale Liu <YLiu@lcs.on.ca>
2023-11-03 09:25:24 -0400

    Created CircleButtonView as a helper to make ContentView shorter.

---
+++ Stop Wacth/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  Stop Wacth
+//
+//  Created by Yiren LIU on 2023-11-03.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
---
+++ Stop Wacth/CircularButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularButtonView.swift
+//  Stop Wacth
+//
+//  Created by Yiren LIU on 2023-11-03.
+//
+
+import SwiftUI
+
+struct CircularButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularButtonView()
+}
